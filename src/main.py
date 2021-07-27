7488b29b4e3c741e05638fafe45c15f44d5d7787
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2021-07-27 12:57:57 +0300

    workspace id

--- src/main.py
+++ src/main.py
@@ -59,7 +59,7 @@ def import_pascal_voc(api: sly.Api, task_id, context, state, app_logger):
     progress_project_cb = init_ui_progress.get_progress_cb(api, task_id, f'Uploading project"', total_files)
     res_project_id, res_project_name = sly.upload_project(dir=proj_dir,
                                                           api=api,
-                                                          workspace_id=g.workspace_id,
+                                                          workspace_id=state["workspaceId"],
                                                           project_name=state["resultingProjectName"],
                                                           log_progress=False,
                                                           progress_cb=progress_project_cb)
