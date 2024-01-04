d81f94f2f86cf56f37cb546122301788f9f7d8c6
almaz <almazgimaev@gmail.com>
2024-01-04 12:37:42 +0100

    back to original links

--- src/globals.py
+++ src/globals.py
@@ -19,9 +19,9 @@ workspace_id = sly.env.workspace_id()
 storage_dir = os.path.join(my_app.data_dir, "pascal_importer")
 sly.fs.mkdir(storage_dir, remove_content_if_exists=True)
 
-# pascal_train_val_dl_link = "http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar"
-pascal_train_val_dl_link = (
-    "http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar"
-)
-# pascal_test_dl_link = "http://pjreddie.com/media/files/VOC2012test.tar"
-pascal_test_dl_link = "http://host.robots.ox.ac.uk:8080/eval/downloads/VOC2012test.tar"
+pascal_train_val_dl_link = "http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar"
+# pascal_train_val_dl_link = (
+#     "http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar"
+# )
+pascal_test_dl_link = "http://pjreddie.com/media/files/VOC2012test.tar"
+# pascal_test_dl_link = "http://host.robots.ox.ac.uk:8080/eval/downloads/VOC2012test.tar"
